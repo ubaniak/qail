74b7bf479082ad9b0b06829e118a35597a0cdd0c
ubaniak <bhavek.budhia@gmail.com>
2025-03-09 13:59:11 +0000

    added the train

--- internal/forms/spinner.go
+++ internal/forms/spinner.go
@@ -4,6 +4,20 @@ import (
 	"github.com/charmbracelet/huh/spinner"
 )
 
+type AnimatedSpinner struct {
+	Frames []string
+	FPS    time.Duration
+}
+
+var Train = AnimatedSpinner{
+	Frames: []string{"      🚂  ",
+		"   🚂 🚃  ",
+		"🚂 🚃 🚃  ",
+		"🚃 🚃     ",
+		"🚃        "},
+	FPS: time.Second / 2,
+}
+
 func Spinner(action func(), message string) error {
-	return spinner.New().Title(message).Action(action).Run()
+	return spinner.New().Title(message).Type(spinner.Type(Train)).Action(action).Run()
 }
